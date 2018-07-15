0bae5568e9868ac46c0e4ac80e1fe04ce798f757
t-sin <shinichi.tanaka45@gmail.com>
2018-07-15 16:36:24 +0900

    Implement chapter1 scheme (without dynamic binding and shallow binding)

--- chap1/lisp.scm
+++ chap1/lisp.scm
@@ -1,8 +1,13 @@
+(import (rnrs base (6)))
+(import (rnrs exceptions (6)))
 (import (rnrs syntax-case (6)))
+(import (rnrs mutable-pairs (6)))
 
 ;;; constants
 
 (define the-false-value (cons "false" "boolean"))
+(define env.init '())
+(define env.global env.init)
 
 ;;; utilities
 
@@ -21,6 +26,19 @@
           (update! id (cdr env) value))
       (raise "No such binding (update!)")))
 
+(define (extend env variables values)
+  (cond ((pair? variables)
+         (if (pair? values)
+             (cons (cons (car variables) (car values))
+                   (extend env (cdr variables) (cdr values)))
+             (raise "Too less values")))
+        ((null? variables)
+          (if (null? values)
+              env
+              (raise "Too much values")))
+         ((symbol? variables)
+          (cons (cons variables values) env))))
+
 ;; evaluate progn
 (define (eprogn exps env)
   (if (pair? exps)
@@ -30,7 +48,9 @@
           (evaluate (car exps) env))
       '()))
 
-(define (make-function args body env) ())
+(define (make-function variables body env)
+  (lambda (values)
+    (eprogn body (extend env.init variables values))))
 
 ;; evaluate argument list
 (define (evlis exps env)
@@ -39,7 +59,41 @@
             (evlis (cdr exps) env))
       '()))
 
-(define (invoke fn args) ())
+(define (invoke fn args)
+  (if (procedure? fn)
+      (fn args)
+      (riase "Not a function")))
+
+;;; global environment
+
+(define-syntax definitial
+  (syntax-rules ()
+    ((definitial name)
+     (begin (set! env.global (cons (cons 'name 'void) env.global))
+            'name))
+    ((definitial name value)
+     (begin (set! env.global (cons (cons 'name value) env.global))
+            'name))))
+
+(define-syntax defprimitive
+  (syntax-rules ()
+    ((defprimitive name value arity)
+     (definitial name 
+       (lambda (values)
+         (if (= arity (length values))
+             (apply value values)
+             (raise "Incorrect arity")))))))
+
+(definitial t #t)
+(definitial f the-false-value)
+(definitial nil '())
+
+(defprimitive cons cons 2)
+(defprimitive car car 1)
+(defprimitive set-cdr! set-cdr! 2)
+(defprimitive + + 2)
+(defprimitive eq? eq? 2)
+(defprimitive < < 2)
 
 ;;; evaluator
 
@@ -60,3 +114,10 @@
         ((lambda) (make-function (car (cdr exp)) (cdr (cdr exp)) env))
         (else (invoke (evaluate (car exp) env)
                       (evlis (cdr exp) env))))))
+
+;;; repl
+
+(define (chap1-toplevel)
+  (begin (display (evaluate (read) env.global))
+         (display "\n"))
+  (chap1-toplevel))
